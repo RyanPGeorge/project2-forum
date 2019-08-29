9ac0c5cd32986fec13f1e98a71657fd3bb64d542
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-08-29 14:13:59 -0500

    final commit before styling

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,7 +6,8 @@ module.exports = {
 
 function create(req, res) {
   Post.findById(req.params.id, function(err, post) {
-    post.comments.push(req.body);
+    post.comments.user = req.body.user;
+    post.comments.push(req.body, req.params.id);
     post.save(function(err) {
       res.redirect(`/posts/${post._id}`);
     });
